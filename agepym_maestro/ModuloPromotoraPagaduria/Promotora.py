72903825c5d8cb0e40383c1c018db4656f6aa130
Infocentro <ms10110@infocentro.com>
2013-08-26 17:56:15 -0600

    Creacion de mi rama, y creacion de modulo promotora y pagaduria

---
+++ agepym_maestro/ModuloPromotoraPagaduria/Promotora.py
@@ -0,0 +1,17 @@
+'''
+Created on 26/08/2013
+
+@author: usuario
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
