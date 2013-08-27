d8d99cb4f076f68e9563a453fe21511416fd057e
kathCLM <ktrin.chris@gmail.com>
2013-08-27 11:58:33 -0600

    Ventanas Login, Gestion Promotoras, Gestion Pagadurias..

---
+++ agepym_maestro/ModuloUsuarios/Login.py
@@ -0,0 +1,60 @@
+'''
+Created on 27/08/2013
+
+@author: Kath
+'''
+
+from PySide import QtCore # @UnresolvedImport
+from PySide.QtGui import QMainWindow, QApplication # @UnresolvedImport
+from Ventanas.LoginWindow import Ui_LogInWindow
+from ModuloUsuarios.Sesion import Sesion
+import sys
+
+class LoginWindow(QMainWindow, Ui_LogInWindow):
+    """
+        Clase controladora de la ventana Login
+    """
+
+    def __init__(self):
+        super(LoginWindow, self).__init__()
+        self.setupUi(self)
+        self.sesion = Sesion()
+        #conecciones 
+        self.connect(self.aceptarBtn, QtCore.SIGNAL('clicked()'), self.aceptar)
+        self.connect(self.cancelarBtn, QtCore.SIGNAL('clicked()'), self.cancelar)
+        self.connect(self.usuarioTxt, QtCore.SIGNAL('editingFinished()'), self.cargarImagen)
+        
+    def cargarImagen(self):
+        #self.label.setText("Carga de imagen")
+        nombre = self.usuarioTxt.text()
+        self.sesion = Sesion(username=nombre)
+        if self.sesion._esValida == True :
+            self.fotoLbl.setPixmap(self.sesion.Usuario.foto)
+        
+    def aceptar(self):
+        #verificacion password
+        #self.label.setText("Aceptar")
+        password = self.contraTxt.text()
+        self.sesion.verificarPassword(password)
+        if self.sesion.esValida() :
+            self.label.setText("Usuario valido")
+            self.limpiar()
+            #redirigir a ventana principal, con sesion
+
+    def cancelar(self):
+        self.limpiar()
+    
+    def limpiar(self):
+        self.usuarioTxt.setText(" ")
+        self.contraTxt.setText(" ")
+        
+def pruebaLogin():
+    print("INICIO DE PRUEBAS\n")
+    app = QApplication(sys.argv)
+    frame = LoginWindow()
+    frame.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    pruebaLogin() 
+        
---
+++ agepym_maestro/Ventanas/LoginWindow.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Chris\Desktop\CICLO VIII\DSI\Windows\LoginWindow.ui'
+#
+# Created: Sat Aug 24 16:59:07 2013
+#      by: pyside-uic 0.2.14 running on PySide 1.2.0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_LogInWindow(object):
+    def setupUi(self, LogInWindow):
+        LogInWindow.setObjectName("LogInWindow")
+        LogInWindow.resize(640, 404)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(LogInWindow.sizePolicy().hasHeightForWidth())
+        LogInWindow.setSizePolicy(sizePolicy)
+        LogInWindow.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.centralwidget = QtGui.QWidget(LogInWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.widget = QtGui.QWidget(self.centralwidget)
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtGui.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.fotoLbl = QtGui.QLabel(self.widget)
+        self.fotoLbl.setText("")
+        self.fotoLbl.setPixmap(QtGui.QPixmap("archivos/imagenes/images.jpg"))
+        self.fotoLbl.setObjectName("fotoLbl")
+        self.gridLayout.addWidget(self.fotoLbl, 1, 1, 1, 1)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 1, 0, 1, 1)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 1, 2, 1, 1)
+        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem2, 0, 1, 1, 1)
+        spacerItem3 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem3, 2, 1, 1, 1)
+        self.verticalLayout_2.addWidget(self.widget)
+        spacerItem4 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem4)
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.usuarioTxt = QtGui.QLineEdit(self.centralwidget)
+        self.usuarioTxt.setObjectName("usuarioTxt")
+        self.verticalLayout_2.addWidget(self.usuarioTxt)
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.contraTxt = QtGui.QLineEdit(self.centralwidget)
+        self.contraTxt.setObjectName("contraTxt")
+        self.verticalLayout_2.addWidget(self.contraTxt)
+        spacerItem5 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem5)
+        self.widget_2 = QtGui.QWidget(self.centralwidget)
+        self.widget_2.setObjectName("widget_2")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.widget_2)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.aceptarBtn = QtGui.QPushButton(self.widget_2)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("archivos/imagenes/ok.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.aceptarBtn.setIcon(icon)
+        self.aceptarBtn.setObjectName("aceptarBtn")
+        self.horizontalLayout.addWidget(self.aceptarBtn)
+        self.cancelarBtn = QtGui.QPushButton(self.widget_2)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("archivos/imagenes/cancel.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.cancelarBtn.setIcon(icon1)
+        self.cancelarBtn.setObjectName("cancelarBtn")
+        self.horizontalLayout.addWidget(self.cancelarBtn)
+        self.verticalLayout_2.addWidget(self.widget_2)
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        LogInWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(LogInWindow)
+        QtCore.QMetaObject.connectSlotsByName(LogInWindow)
+
+    def retranslateUi(self, LogInWindow):
+        LogInWindow.setWindowTitle(QtGui.QApplication.translate("LogInWindow", "MAESTRO - Iniciar Sesión", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("LogInWindow", "Usuario:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("LogInWindow", "Contraseña: ", None, QtGui.QApplication.UnicodeUTF8))
+        self.aceptarBtn.setText(QtGui.QApplication.translate("LogInWindow", "Aceptar", None, QtGui.QApplication.UnicodeUTF8))
+        self.cancelarBtn.setText(QtGui.QApplication.translate("LogInWindow", "Cancelar", None, QtGui.QApplication.UnicodeUTF8))
+
